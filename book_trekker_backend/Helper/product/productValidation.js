2985959bd23f10285383ef9ea73af5d0fd6176ad
engrmzubair <mz3970884@gmail.com>
2022-05-15 11:28:39 +0500

    product Controller refactoring

---
+++ book_trekker_backend/Helper/product/productValidation.js
@@ -0,0 +1,71 @@
+const fs = require('fs');
+const multer = require('multer')
+
+//imports from other local directories
+const AppError = require("../../utils/appError");
+const { validate } = require('../../models/productModel');
+const { Category } = require('../../models/categoryModel');
+
+
+// ..........validation............
+exports.fieldsValidation = (req, res, next) => {
+
+  // productData object added to req
+  req.productData = req.file && req.body && { ...req.body, photo: req.file.filename };
+
+  // fields validation
+  const { error } = validate(req.productData);
+
+  //if error => throw it to global error handler
+  if (error) {
+    fs.unlinkSync(req.file.path);
+    next(new AppError(error.details[ 0 ].message, 400))
+  }
+}
+
+//category validation
+exports.categoryExists = async (req, res, next) => {
+  try {
+    const category = await Category.findOne({ _id: req.productData.category }).exec();
+
+    if (category && category.name) return next()
+    fs.unlinkSync(req.file.path);
+    return next(new AppError('Category does not exists!', 404))
+  } catch (ex) {
+    next(ex)
+  }
+}
+
+//multer storage
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './public/products')
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    const ext = file.mimetype.split('/')[ 1 ];
+    cb(null, file.fieldname + '-' + uniqueSuffix + "." + ext)
+  }
+})
+
+//multer file filter (only images are allowed)
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg") {
+    cb(null, true);
+  } else {
+    cb(null, false);
+    return cb(new AppError('Only .png, .jpg and .jpeg format allowed!', 400));
+  }
+}
+
+//file size limit to 1MB
+const maxSize = 1 * 1024 * 1024;
+const limits = { fileSize: maxSize }
+
+//multer option objects
+exports.multerOptions = {
+  storage: storage,
+  fileFilter,
+  limits
+}
+
--- book_trekker_backend/controllers/productController.js
+++ book_trekker_backend/controllers/productController.js
@@ -1,43 +1,17 @@
-// const fs = require('fs');
 const multer = require('multer')
 
+//imports from other local directories
 const catchAsync = require("../utils/catchAsync");
-const { Product, validate } = require('../models/productModel');
+const { Product } = require('../models/productModel');
 const AppError = require("../utils/appError");
 const path = require('path');
-const fs = require('fs');
-const { Category } = require('../models/categoryModel');
+const {
+  fieldsValidation,
+  categoryExists,
+  multerOptions
+} = require('../Helper/product/productValidation');
 
-
-// ..........validation............
-const fieldsValidation = (req, res, next) => {
-
-  // productData object added to req
-  req.productData = req.file && req.body && { ...req.body, photo: req.file.filename };
-
-  // fields validation
-  const { error } = validate(req.productData);
-
-  //if error => throw it to global error handler
-  if (error) {
-    fs.unlinkSync(req.file.path);
-    next(new AppError(error.details[ 0 ].message, 400))
-  }
-}
-
-//category validation
-const categoryExists = async (req, res, next) => {
-  try {
-    const category = await Category.findOne({ _id: req.productData.category }).exec();
-    if (category && category.name) return next()
-    fs.unlinkSync(req.file.path);
-    return next(new AppError('Category does not exists!', 404))
-  } catch (ex) {
-    next(ex)
-  }
-}
-
-//product validation
+//product validation from client side
 exports.productValidation = async (req, res, next) => {
 
   if (!req.file) next(new AppError('Please attach the product image!', 400));
@@ -47,42 +21,10 @@ exports.productValidation = async (req, res, next) => {
 
   //category validation
   categoryExists(req, res, next);
-
 }
-
-//multer storage
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './public/products')
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-    const ext = file.mimetype.split('/')[ 1 ];
-    cb(null, file.fieldname + '-' + uniqueSuffix + "." + ext)
-  }
-})
-
-//multer file filter (only images are allowed)
-const fileFilter = (req, file, cb) => {
-  if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg") {
-    cb(null, true);
-  } else {
-    cb(null, false);
-    return cb(new AppError('Only .png, .jpg and .jpeg format allowed!', 400));
-  }
-}
-
-//file size limit to 1MB
-const maxSize = 1 * 1024 * 1024;
-const limits = { fileSize: maxSize }
 
 //multer middleware for parsing multipart form data
-exports.uploadProductPhoto = multer({
-  storage: storage,
-  fileFilter,
-  limits
-}).single('photo');
-
+exports.uploadProductPhoto = multer(multerOptions).single('photo');
 
 
 //route handler for create product

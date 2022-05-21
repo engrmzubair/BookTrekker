0dc8ee4db12f9d7a0f4628a1e14a4313250405c4
engrmzubair <mz3970884@gmail.com>
2022-05-21 08:40:01 +0500

    react app with redux typescript template added

---
+++ book-trekker-app/src/App.tsx
@@ -0,0 +1,13 @@
+import { Counter } from './features/counter/Counter';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate ipsum quidem autem animi excepturi fugiat dolor voluptatum nisi quaerat consequuntur, neque numquam cumque saepe temporibus nulla non asperiores doloremque ducimus?</p>
+    </div>
+  );
+}
+
+export default App;
